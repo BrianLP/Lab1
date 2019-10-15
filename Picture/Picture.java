d7b0c461eb6c0c77cf09de32ac64e7c04cc2d2d8
Brian Patterson <brian@zipcoders-MacBook-Pro-5.local>
2019-10-15 18:01:54 -0400

    start of ppicture

--- Picture/Picture.java
+++ Picture/Picture.java
@@ -13,9 +13,19 @@
 public class Picture
 {
     private Square wall;
+    private Square wall2;
+    private Square floor;
+    private Square sky;
+    private Square roof;
+    private Square roof2;
+    private Square roof3;
+    private Square roof4;
+    private Square roof5;
     private Square window;
-    private Triangle roof;
+    private Triangle roofedge;
+    private Triangle roofedge2;
     private Circle sun;
+    private Triangle walkway;
 
     /**
      * Constructor for objects of class Picture
@@ -30,28 +40,99 @@ public class Picture
      */
     public void draw()
     {
+        
+        
         wall = new Square();
-        wall.moveVertical(80);
-        wall.changeSize(100);
+        wall.moveHorizontal(60);
+        wall.moveVertical(130);
+        wall.changeSize(75);
         wall.makeVisible();
-
+        
+        wall2 = new Square();
+        wall2.moveHorizontal(135);
+        wall2.moveVertical(130);
+        wall2.changeSize(75);
+        wall2.makeVisible();
+        
+        roof = new Square();
+        roof.moveHorizontal(175);
+        roof.moveVertical(115);
+        roof.changeSize(30);
+        roof.changeColor("black");
+        roof.makeVisible();
+        
+        roof2 = new Square();
+        roof2.moveHorizontal(65);
+        roof2.moveVertical(115);
+        roof2.changeSize(30);
+        roof2.changeColor("black");
+        roof2.makeVisible();
+        
+        roof3 = new Square();
+        roof3.moveHorizontal(120);
+        roof3.moveVertical(115);
+        roof3.changeSize(30);
+        roof3.changeColor("black");
+        roof3.makeVisible();
+        
+        roof4 = new Square();
+        roof4.moveHorizontal(95);
+        roof4.moveVertical(115);
+        roof4.changeSize(30);
+        roof4.changeColor("black");
+        roof4.makeVisible();
+        
+        roof5 = new Square();
+        roof5.moveHorizontal(145);
+        roof5.moveVertical(115);
+        roof5.changeSize(30);
+        roof5.changeColor("black");
+        roof5.makeVisible();
+        
+        window = new Square();
+        window.changeColor("black");
+        window.moveHorizontal(20);
+        window.moveVertical(100);
+        window.makeVisible();
+        
+        window = new Square();
+        window.changeColor("black");
+        window.moveHorizontal(20);
+        window.moveVertical(100);
+        window.makeVisible();
+        
+        window = new Square();
+        window.changeColor("black");
+        window.moveHorizontal(20);
+        window.moveVertical(100);
+        window.makeVisible();
+        
         window = new Square();
         window.changeColor("black");
         window.moveHorizontal(20);
         window.moveVertical(100);
         window.makeVisible();
 
-        roof = new Triangle();
-        roof.changeSize(50, 140);
-        roof.moveHorizontal(60);
-        roof.moveVertical(70);
+        roofedge = new Triangle();
+        roofedge.changeSize(30,40);
+        roofedge.moveHorizontal(65);
+        roofedge.moveVertical(115);
+        roofedge.changeColor("black")
         roof.makeVisible();
+       
+        roofedge2 = new Triangle();
+        roofedge2.changeSize(30,40);
+        roofedge2.moveHorizontal(205);
+        roofedge2.moveVertical(115);
+        roofedge2.changeColor("black")
+        roofedge2.makeVisible();
+       
 
         sun = new Circle();
         sun.changeColor("yellow");
-        sun.moveHorizontal(180);
-        sun.moveVertical(-10);
-        sun.changeSize(60);
+        sun.moveHorizontal(20);
+        sun.moveVertical(20);
+        sun.changeSize(35);
         sun.makeVisible();
     }
 
